599f526d0a5b5f9d2c95093ab2ea52547c5e8d47
Timo Rintala <timorintalatr@gmail.com>
2017-09-08 17:02:28 +0300

    -Added simple map to test stuff on + couple placeholder graphics. -Added player movement + raycasts to eliminate the glitchiness of the movement next to walls. -Added three different camera modes to test on all of them.

---
+++ TDS/Assets/Scripts/CameraSmooth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmooth : MonoBehaviour {
+
+    public Transform objectToLookAt;
+    public bool smooth = true;
+    public bool smoothAimZoom = true;
+    public bool smoothAimZoomSpecial = true;
+    public float smoothSpeed = 0.15f;
+    private Camera main;
+    private GameObject player;
+    private float defaultOrthographicSize;
+
+    private void Awake() {
+        main = GetComponent<Camera>();
+        player = GameObject.Find("Player");
+        defaultOrthographicSize = main.orthographicSize;
+    }
+
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+
+	private void LateUpdate () {
+        Vector3 desiredPosition = objectToLookAt.transform.position + offset;
+
+        if (smooth) {
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        } else if (smoothAimZoom) {
+            Vector3 mouseScreenPosition = main.ScreenToWorldPoint(Input.mousePosition);
+
+            transform.position = Vector3.Lerp(objectToLookAt.position, mouseScreenPosition + offset, 0.25f);
+        } else if (smoothAimZoomSpecial) {
+            Vector3 mouseScreenPosition = main.ScreenToWorldPoint(Input.mousePosition);
+            float distance = Vector3.Distance(mouseScreenPosition, player.transform.position);
+
+            transform.position = Vector3.Lerp(objectToLookAt.position, mouseScreenPosition + offset, 0.1f);
+            Debug.Log(distance);
+            if (distance <= 8f) {
+                main.orthographicSize = 12f;
+            } else {
+                main.orthographicSize = defaultOrthographicSize + ((distance-8f) / 10f);
+                if (main.orthographicSize > 15.0f) {
+                    main.orthographicSize = 15.0f;
+                }
+            }
+            
+        }
+        else {
+            transform.position = desiredPosition;
+        }
+	}
+}
---
+++ TDS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //MouseLook
+    public Camera main;
+
+
+    public float movementSpeed;
+    public bool drawPlayerGizmos = true;
+
+    //Raycasting and movement
+    private bool disableTop = false;
+    private bool disableBottom = false;
+    private bool disableLeft = false;
+    private bool disableRight = false;
+
+    void Update () {
+        Movement();
+        MouseLook();
+        WallCheck();
+	}
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D) && !disableTop && !disableRight) {
+            transform.position += new Vector3(1f, 1f).normalized * movementSpeed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A) && !disableTop && !disableLeft) {
+            transform.position += new Vector3(-1f, 1f).normalized * movementSpeed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D) && !disableBottom && !disableRight) {
+            transform.position += new Vector3(1f, -1f).normalized * movementSpeed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A) && !disableBottom && !disableLeft) {
+            transform.position += new Vector3(-1f, -1f).normalized * movementSpeed * Time.deltaTime;
+        } else {
+            if (Input.GetKey(KeyCode.W) && !disableTop) {
+                transform.position += new Vector3(0f, 1f) * movementSpeed * Time.deltaTime;
+            }
+             else if (Input.GetKey(KeyCode.S) && !disableBottom) {
+                transform.position += new Vector3(0f, -1f) * movementSpeed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.A) && !disableLeft) {
+                transform.position += new Vector3(-1f, 0f) * movementSpeed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D) && !disableRight) {
+                transform.position += new Vector3(1f, 0f) * movementSpeed * Time.deltaTime;
+            }
+        }
+    }
+
+    void MouseLook() {
+        // convert mouse position into world coordinates
+        Vector2 mouseScreenPosition = main.ScreenToWorldPoint(Input.mousePosition);
+
+        // get direction you want to point at
+        Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+
+        // set vector of transform directly
+        transform.right = direction;
+    }
+
+    void WallCheck() {
+        if (Physics2D.Raycast(transform.position, Vector3.up, 1f, 1 << LayerMask.NameToLayer("Wall"))) {
+            disableTop = true;
+        } else {
+            disableTop = false;
+        }
+        if (Physics2D.Raycast(transform.position, Vector3.down, 1f, 1 << LayerMask.NameToLayer("Wall"))) {
+            disableBottom = true;
+        } else {
+            disableBottom = false;
+        }
+        if (Physics2D.Raycast(transform.position, Vector3.left, 1f, 1 << LayerMask.NameToLayer("Wall"))) {
+            disableLeft = true;
+        } else {
+            disableLeft = false;
+        }
+        if (Physics2D.Raycast(transform.position, Vector3.right, 1f, 1 << LayerMask.NameToLayer("Wall"))) {
+            disableRight = true;
+        } else {
+            disableRight = false;
+        }
+    }
+
+    private void OnDrawGizmos() {
+        if (drawPlayerGizmos) {
+            Gizmos.color = Color.green;
+
+            //Top
+            Gizmos.DrawRay(transform.position, Vector3.up);
+            //Bottom
+            Gizmos.DrawRay(transform.position, Vector3.down);
+            //Left
+            Gizmos.DrawRay(transform.position, Vector3.left);
+            //Right
+            Gizmos.DrawRay(transform.position, Vector3.right);
+        }
+    }
+}
