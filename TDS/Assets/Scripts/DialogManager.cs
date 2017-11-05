9710557bc9024eadad8563f12a55d52f7a279c9d
Timo Rintala <timorintalatr@gmail.com>
2017-11-05 14:36:41 +0200

    -Added dialoguemanager (WIP)

---
+++ TDS/Assets/Scripts/DialogManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class DialogManager : MonoBehaviour {
+
+    public FileInfo sourceFile;
+    public StreamReader reader;
+    public string text;
+    public List<string> dialogueList;
+
+    private void Awake() {
+        dialogueList = new List<string>();
+        sourceFile = new FileInfo("Assets/Text/Dialogue.txt");
+        reader = sourceFile.OpenText();
+
+        while (text != null) {
+            text = reader.ReadLine();
+
+            if (text != null) {
+                dialogueList.Add(text);
+            }
+            
+        }
+    }
+
+    public string GetStringFromList(string keyString) {
+        for (int i = 0; i < dialogueList.Count; i++) {
+            if (dialogueList[i].Contains(keyString)) {
+                string finaltext = dialogueList[i].Substring(keyString.Length + 1);
+                return finaltext;
+            }
+        }
+        return "ERROR: CANT FIND THE STRING.";
+    }
+}
