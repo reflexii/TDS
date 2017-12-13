01c9a98e2c0bdd321d3216a845cc10b9bd822f3c
WPK\c5tirint <timorintalatr@gmail.com>
2017-12-13 14:14:33 +0200

    -Cutscenestuff

---
+++ TDS/Assets/Scripts/CutSceneEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneEnemy : MonoBehaviour {
+
+    public float movementSpeed = 5f;
+    public bool horizontal = true;
+    public bool walking = true;
+
+    private Animator animator;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+
+        Animations();
+
+        if (walking) {
+            if (horizontal) {
+                gameObject.transform.position += new Vector3(1f, 0f, 0f) * movementSpeed * Time.deltaTime;
+            } else {
+                gameObject.transform.position += new Vector3(0f, -1f, 0f) * movementSpeed * Time.deltaTime;
+            }
+        }
+        
+	}
+
+    void Animations() {
+        animator.SetBool("Walking", walking);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("EnemyWalk")) {
+            walking = false;
+        }
+    }
+}
